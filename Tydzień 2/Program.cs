0a6c157c5c236f1c0e1c971d611957eefee56d36
Edi0501 <edytanowakowska05@gmail.com>
2023-12-11 10:57:11 +0100

    Add project files.

---
+++ Tydzień 2/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Tydzień2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Uzupełnij formularz");
+            Console.WriteLine("Imię");
+            {
+                string imię = "Adam";
+                Console.ReadLine();
+            }
+            Console.WriteLine("Nazwisko");
+            {
+                string nazwisko = "Kowalski";
+                Console.ReadLine();
+            }
+            Console.WriteLine("Wiek");
+            {
+                int wiek = 30;
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Płeć, k lub m");
+            {
+                char płeć = 'm';
+                Console.ReadLine();
+            }
+            Console.WriteLine("Numer PESEL");
+            {
+                string NumerPESEL = "80010106174";
+                Console.ReadLine();
+            }
+            Console.WriteLine("Numer pracownika");
+            {
+                string NumerPracownika = "987654321";
+                Console.ReadLine();
+            }
+            
+            
+        }
+    }
+}
+
